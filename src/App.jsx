41510891349d66d8d8665f549c53a816322d736d
Yasaman Abedi <Yasaman471@gmail.com>
2026-01-30 20:15:42 +0330

    feat:set up base routing structure and Navigation Implements the initial routing configuration for the application using React Router. Defines the main application flow, including navigation to product listings, product details, checkout, and a fallback 404 page. Sets the product listing as the default landing page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import ProductPage from "./Pages/ProductPage";
+import DetailesPage from "./Pages/DetailesPage";
+import CheckOutPage from "./Pages/CheckOutPage";
+import PageNotFound from "./Pages/404";
+
 function App() {
   return (
     <>
-      <h1>App</h1>
+      <Routes>
+        <Route index element={<Navigate to="/products" replace />} />
+        <Route path="/products" element={<ProductPage />} />
+        <Route path="/products/:id" element={<DetailesPage />} />
+        <Route path="/checkout" element={<CheckOutPage />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
     </>
   );
 }
