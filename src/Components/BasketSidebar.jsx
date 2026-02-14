06acff030d27ce192296348cf6ba9e22311315cf
Yasaman Abedi <Yasaman471@gmail.com>
2026-02-14 10:55:13 +0330

    feat:Implement BasketSideBar component structure and add style

---
+++ src/Components/BasketSidebar.jsx
@@ -0,0 +1,30 @@
+import { BsPatchCheck } from "react-icons/bs";
+import { FaHashtag } from "react-icons/fa";
+import { TbChecklist } from "react-icons/tb";
+
+import styles from "./BasketSidebar.module.css";
+
+function BasketSidebar({ state, clickHandler }) {
+  return (
+    <div className={styles.sidebar}>
+      <div>
+        <TbChecklist />
+        <p>Total:</p>
+        <span>{state.totalPrice} $</span>
+      </div>
+      <div>
+        <FaHashtag />
+        <p>Quantity:</p>
+        <span>{state.totalQuantity}</span>
+      </div>
+      <div>
+        <BsPatchCheck />
+        <p>Status:</p>
+        <span>{!state.checkout && "Pending..."}</span>
+      </div>
+      <button onClick={() => clickHandler("CHECKOUT")}>CheckOut</button>
+    </div>
+  );
+}
+
+export default BasketSidebar;
--- src/Pages/CheckOutPage.jsx
+++ src/Pages/CheckOutPage.jsx
@@ -1,5 +1,10 @@
+import { IoBagHandleOutline } from "react-icons/io5";
+
 import BasketCard from "../Components/BasketCard";
 import { useCart } from "../Context/CartContext";
+
+import styles from "./CheckOutPage.module.css";
+import BasketSidebar from "../Components/BasketSidebar";
 
 function CheckOutPage() {
   const [state, dispatch] = useCart();
@@ -8,15 +13,25 @@ function CheckOutPage() {
 
   return (
     <div>
-      <div>
-        {state.selectedItems.map((product) => (
-          <BasketCard
-            key={product.id}
-            data={product}
-            clickHandler={clickHandler}
-          />
-        ))}
-      </div>
+      {!state.totalQuantity ? (
+        <div className={styles.emptyCartContainer}>
+          <p>Your Card Is Empty!!</p>
+          <IoBagHandleOutline />
+        </div>
+      ) : (
+        <div className={styles.container}>
+          <BasketSidebar state={state} clickHandler={clickHandler} />
+          <div className={styles.products}>
+            {state.selectedItems.map((product) => (
+              <BasketCard
+                key={product.id}
+                data={product}
+                clickHandler={clickHandler}
+              />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
